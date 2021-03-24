06f9c4ea6313d044eb7baff2a723d2af9f5a2a24
Florencia Eusebi <eusebiflorencia@gmail.com>
2021-03-24 17:41:03 -0300

    BQA-08: file name changed

---
+++ index.js
@@ -0,0 +1,9 @@
+/****** Variables and Operators *******/
+/****** Exrcise A *******/
+var workingDaysFirstSemester, workingDaysSecondSemester, daysSum;
+
+workingDaysFirstSemester = 40;
+workingDaysSecondSemester = 35;
+
+daysSum = workingDaysFirstSemester + workingDaysSecondSemester;
+console.log(daysSum);
