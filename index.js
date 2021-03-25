023aaaf7671353f0d0a617c9b4902ccb54aa8844
Florencia Eusebi <eusebiflorencia@gmail.com>
2021-03-25 00:01:13 -0300

    BQA-08: Exercise 2 A

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/****** Variables and Operators *******/
-/****** Exercise A *******/
+//****** Variables and Operators *******
+//****** Exercise 1 A *******
   var workingDaysFirstSemester, workingDaysSecondSemester, daysSum;
 
   workingDaysFirstSemester = 40;
@@ -9,4 +9,32 @@
 
   console.log(daysSum);
 
+//****** Exercise 1 B ******
+  var courseName, teacherName, courseInformation;
 
+  courseName = 'First Certificate';
+  teacherName = 'Juana Perez';
+  console.log('The course is: ' + courseName + ', and the teacher is ' + teacherName);
+
+  courseInformation = courseName + teacherName;
+  console.log(courseInformation);
+
+//****** Exercise 1 C ******
+  var coursePresents, courseAbsents, presentsAbsentsSum;
+
+  coursePresents = 'PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP';
+  courseAbsents = 'AAAAAAAAAA'
+  presentsAbsentsSum = coursePresents.length + courseAbsents.length;
+
+  console.log('The total of lessons delivered is ' + presentsAbsentsSum);
+
+//****** Strings ******
+//****** Exercise 2 A *******
+  var courseObjective;
+
+  courseObjective =  'To prepare students for the First Certificate Exam delivered by Cambridge University'
+  console.log('To prepare students for the First Certificate Exam delivered by Cambridge University'.toUpperCase(courseObjective));
+
+//****** Exercise 2 B ******
+  var numberOfStudents;
+  numberOfStudents = 'There are ten students enrolled in the First Cerificate course';
